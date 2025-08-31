76cca5af03fc61a16c1248c744845772430efc8e
hichambenguesmia00-byte <hichambenguesmia00@gmail.com>
2025-08-31 15:59:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 from flask import Flask
-import threading
-from checker import run_checker
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "✅ BLS slot checker is running!"
-
-def start_checker():
-    run_checker()
+    return "✅ Flask is running on Render!"
 
 if __name__ == "__main__":
-    # نشغل المراقبة في thread ثاني
-    threading.Thread(target=start_checker, daemon=True).start()
-    
-    # Flask لازم يربط على 0.0.0.0 باش Render يقبل الخدمة
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=10000)  # Render يسمع على أي بورت
