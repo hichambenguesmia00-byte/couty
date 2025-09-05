c9de862d063768d6ee850bac6133f5d8f9479a5f
hichambenguesmia00-byte <hichambenguesmia00@gmail.com>
2025-09-05 18:49:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 import threading
 import requests
 from flask import Flask
-from telegram.ext import Updater, CommandHandler
+from telegram import Update
+from telegram.ext import Updater,CommandHandler, CallbackContext
 
 # متغيرات
 COOKIES = {}
@@ -62,6 +63,30 @@ def check(update, context):
     else:
         update.message.reply_text("⏳ مازال مكاينش.")
 
+
+def checkcookies(update: Update, context: CallbackContext):
+    if not COOKIES:
+        update.message.reply_text("⚠️ ما عندكش كوكيز مخزنين. استعمل /setcookies باش تزيدهم.")
+        return
+
+    url = "https://algeria.blsspainglobal.com/DZA/Appointment/NewAppointment?msg=ZokWWxtCWRl2wwydQeR8iMSec%2BFRGm9yoFAG67YF%2FE46MHPKOT4E5B42DNnLtDwr&d=vIl4VHDNjFut2gxJov6ucTev%2Fo864siLsWLuqQOrNmjX70CyvfreOCQkRSP3l98sKS85uaee%2B6ZgvWphouiemjMKWOmpGRJuLnOETWreviSyKxWcXudgMEZduaH%2FCiiiyTH%2Fni8F9z1i9gJBfdIy5LaaF0xP%2F9ZYmO0Qv1i6bKv90KpYGr6tXxH28U955kWbvK9W9fraA98ON3bl%2BHuHr2GOMHOQ1BhqHg5LhvxmxEBfpoZ5XanOcHypferontrbLmKZYSycAWdU3xd%2BjyfXjs0pGgL%2BftFlczaOfLYOMSm6SsqBo086dTopNJNlBJqC"  # 🔴 هنا حط رابط الصفحة لي لازم تتحقق منها
+    try:
+        resp = requests.get(url, cookies=COOKIES, timeout=10)
+
+        if resp.status_code == 200:
+            # تحقق من كلمة في الصفحة تدل على نجاح الدخول
+            if "مرحبا" in resp.text or "Welcome" in resp.text:
+                update.message.reply_text("✅ الكوكيز صالح والدخول للصفحة نجح.")
+            else:
+                update.message.reply_text("⚠️ دخل للصفحة بصح المحتوى ما يبينش باللي صالح.")
+        elif resp.status_code in (401, 403):
+            update.message.reply_text("❌ انتهت صلاحية الكوكيز. لازم تدخل كوكيز جديد.")
+        else:
+            update.message.reply_text(f"❌ خطأ: status code = {resp.status_code}")
+
+    except Exception as e:
+        update.message.reply_text(f"⚠️ خطأ في الاتصال: {str(e)}")
+
 # -------- تشغيل البوت في Thread --------
 def run_bot():
     updater = Updater(BOT_TOKEN, use_context=True)
@@ -72,7 +97,9 @@ def run_bot():
     dp.add_handler(CommandHandler("check", check))
     updater.start_polling()
     updater.idle()
-
+# باش تزيد الكوموند للبوت
+def register_handlers(dispatcher):
+    dispatcher.add_handler(CommandHandler("checkcookies", checkcookies))
 # -------- Route Flask --------
 @app.route("/")
 def home():
